ae8e2fc3cb4621b767bf49fb7ddfd8c0542898f2
Horry Portier <kowalskis354@gmail.com>
2022-06-29 15:15:22 +0200

    added docs to main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from display import PrintSummoner, get_input, load_data
 from op_gg_scraper import Summoner 
 
-
-
 def main():
+    '''gets input from user (defult values: region-kr, player name-hide on bush)'''
+    region , name = get_input() 
 
-    region , name = get_input()   
- 
     '''gets player data''' 
     summoner = Summoner()
     summoner.get_summoner(region=region, sum_name=name)
-    
-    '''op_gg_scraper func'''
+
+    '''op_gg_scraper.py func'''
     summoner.dump_data()
-    '''print  func'''
+    '''display.py  func'''
     data =  load_data()
 
     print_s = PrintSummoner(data)
